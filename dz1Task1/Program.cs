f8e5f923f65ed735d8d0852d01752d3a7cd9f3f0
Rafik Kuliev <rombaks2019@gmail.com>
2023-06-22 22:23:10 +0300

    Add method Task4

--- dz1Task1/Program.cs
+++ dz1Task1/Program.cs
@@ -4,10 +4,14 @@
     {
         static void Main(string[] args)
         {
-            maxNumber();
+            //task2();
+            task4();
         }
 
-        static private void maxNumber()
+        //
+        //Метод принимает два числа и выводит максимальное число
+        //
+        static private void task2()
         {
             Console.Write("Введите первое число: ");
             int numberFirst = Convert.ToInt32(Console.ReadLine());
@@ -16,8 +20,35 @@
             int NumberSecond = Convert.ToInt32(Console.ReadLine());
 
             if (numberFirst > NumberSecond)
-                Console.WriteLine($"Максимальное число =  {numberFirst}") ;
+                Console.WriteLine($"Максимальное число =  {numberFirst}");
             else Console.WriteLine($"Максимальное число =  {NumberSecond}");
+        }
+
+        //
+        //Метод принимает три числа и выдает максимальное из этих чисел
+        //
+        static private void task4()
+        {
+            Console.Write("Введите первое число: ");
+            int numberFirst = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Введите второе число: ");
+            int NumberSecond = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Введите третье число: ");
+            int NumberThird = Convert.ToInt32(Console.ReadLine());
+
+            int max = numberFirst;
+
+            if (max < NumberSecond)
+            {
+                max = NumberSecond;
+            }
+            if (max < NumberThird)
+            {
+                max = NumberThird;
+            }
+            Console.WriteLine(max);
         }
     }
 }
