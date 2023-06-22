706f1c5702efadd67a9b6eb52dc33dcb5176106c
Rafik Kuliev <rombaks2019@gmail.com>
2023-06-22 22:14:41 +0300

    Добавьте файлы проекта.

---
+++ dz1Task1/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace dz1Task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            maxNumber();
+        }
+
+        static private void maxNumber()
+        {
+            Console.Write("Введите первое число: ");
+            int numberFirst = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Введите второе число: ");
+            int NumberSecond = Convert.ToInt32(Console.ReadLine());
+
+            if (numberFirst > NumberSecond)
+                Console.WriteLine($"Максимальное число =  {numberFirst}") ;
+            else Console.WriteLine($"Максимальное число =  {NumberSecond}");
+        }
+    }
+}
